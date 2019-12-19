ddd7eddf7aebe81fd7136d14388cadad0bac945e
CristianPeralta <cbperaltasegura@gmail.com>
2019-12-19 18:32:37 -0500

    add user model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: "Username is required"
+    },
+    email: {
+        type:String,
+        unique: true,
+        required: "Email is required"
+    },
+    password: {
+        type:String,
+        required: "Password is required"
+    },
+    keywords: {
+        type: "Array",
+        default: []
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
