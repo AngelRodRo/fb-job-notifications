ec685bf17735a042f8b641813af234495eed0cb9
Angel <aleangrodriguez123@gmail.com>
2019-12-19 20:00:07 -0500

    logs

--- index.js
+++ index.js
@@ -91,21 +91,21 @@ const getPosts = async (page) => {
   const fbUrl = 'https://facebook.com';
   const page = await browser.newPage();
 
-  const notifier = require('node-notifier');
+  // const notifier = require('node-notifier');
 
-  // String
-  notifier.notify('Go empty the dishwasher!');
+  // // String
+  // notifier.notify('Go empty the dishwasher!');
 
-  // Object
-  notifier.notify({
-    'title': 'David Walsh Blog',
-    'subtitle': 'Daily Maintenance',
-    'message': 'Go approve comments in moderation!',
-    'icon': 'dwb-logo.png',
-    'contentImage': 'blog.png',
-    'sound': 'ding.mp3',
-    'wait': true
-  });
+  // // Object
+  // notifier.notify({
+  //   'title': 'David Walsh Blog',
+  //   'subtitle': 'Daily Maintenance',
+  //   'message': 'Go approve comments in moderation!',
+  //   'icon': 'dwb-logo.png',
+  //   'contentImage': 'blog.png',
+  //   'sound': 'ding.mp3',
+  //   'wait': true
+  // });
   const login = async () => {
     // login
     await page.goto(fbUrl, {
