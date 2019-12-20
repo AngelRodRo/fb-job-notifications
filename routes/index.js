58ede4585e59fe3d4fa4ae463281dccb7d4de549
Angel <aleangrodriguez123@gmail.com>
2019-12-19 19:59:18 -0500

    remove server

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const user = require("../models/user");
-
-router.get('/', function(req, res, next) {
-  res.json('Hello Guys');
-});
-
-module.exports = router;
